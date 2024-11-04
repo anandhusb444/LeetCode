7910d4c7d0148f58f29852e528bbbe6805429400
anandhu sb <anandhusb444@gmail.com>
2024-11-04 19:02:11 +0530

    Time: 0 ms (100%), Space: 45.4 MB (67.39%) - LeetHub

--- 0136-single-number/0136-single-number.cs
+++ 0136-single-number/0136-single-number.cs
@@ -1,10 +1,10 @@
 public class Solution {
     public int SingleNumber(int[] nums) {
         var result = 0;
-        if(nums.Length <= 1)
-        {
-            return nums[0];
-        }
+        // if(nums.Length <= 1)
+        // {
+        //     return nums[0];
+        // }
 
         for(int i=0; i<nums.Length; i++)
         {
